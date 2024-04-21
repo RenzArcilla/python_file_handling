52d7e49aae5144905042ed797ebe94e6e9da4c72
unknown <renzarcilla09@gmail.com>
2024-04-21 12:35:52 +0800

    odd numbers added to odd.txt, even numbers added to even.txt

--- read_and_write_even_or_odd.py
+++ read_and_write_even_or_odd.py
@@ -15,7 +15,11 @@ with open("numbers.txt", "r") as numbers_file:
     for line in numbers_file:
         line = line.rstrip("\n")
         line = int(line)
-        if line % 2 == 0:
-            print("EVEN:", line)
-        else:
-            print("ODD:", line)
+        if line % 2 == 0: #checks if int is even
+            with open("even.txt", "a") as even_numbers_file:
+                even_numbers_file.write(str(line))
+                even_numbers_file.write("\n")
+        else: #checks if int is odd
+            with open("odd.txt", "a") as odd_numbers_file:
+                odd_numbers_file.write(str(line))
+                odd_numbers_file.write("\n")
