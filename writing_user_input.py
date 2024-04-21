03ba04d10317175917b47ee079adf34dc12e69aa
unknown <renzarcilla09@gmail.com>
2024-04-21 20:42:27 +0800

    resized buttons and widgets, changed fonts, sizes, and styles

--- writing_user_input.py
+++ writing_user_input.py
@@ -14,12 +14,13 @@ window.geometry("900x300")
 window.resizable(False, False)
 
 #     display instructions as text on window
-instructions = Label(window, text="Please input a statement on the text box." + "\n" + "Click 'ENTER' to submit a statement." + "\n" + "Click 'EXIT' to close the program.")
+instructions = Label(window, text="Please input a statement on the text box." + "\n" + "Click 'ENTER' to submit a statement." + "\n" + "Click 'EXIT' to close the program.", font=("Times New Roman", 20, "bold"))
 instructions.pack()
 
 #     create an entry widget in window for users to enter statements
 statement_input_widget = Entry()
-statement_input_widget.config(width=50)
+statement_input_widget.config(width=25)
+statement_input_widget.config(font=("Times New Roman", 40))
 statement_input_widget.pack()
 
 #     append the submitted statement on a txt file
@@ -30,11 +31,11 @@ def submit():
         output_file.write("\n")
 
 #     create button for user to submit their statement
-submit_button = Button(text="SUBMIT", command=submit) # submit() is a function, the function is called when button is clicked
+submit_button = Button(text="SUBMIT", font=("MS Serif", 20, "bold"), command=submit) # submit() is a function, the function is called when button is clicked
 submit_button.pack()
 
 #     create an exit button that exits window and terminates the program
-exit_button = Button(text="EXIT", command=quit) # the window is terminated when exit button is clicked
+exit_button = Button(text="EXIT", font=("MS Serif", 20, "bold"), command=quit) # the window is terminated when exit button is clicked
 exit_button.pack()
 
 window.mainloop()
