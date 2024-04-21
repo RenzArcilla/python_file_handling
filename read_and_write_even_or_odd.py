c704364d834843456a3ce45137da2752ce426c15
unknown <renzarcilla09@gmail.com>
2024-04-21 11:12:06 +0800

    wrote pseudocode

---
+++ read_and_write_even_or_odd.py
@@ -0,0 +1,13 @@
+# pseudocode:
+#    create numbers.txt file
+#    write 20 integers in the txt file
+#    open txt file
+#    read txt file per line
+#        strip "\n"
+#        convert str to int
+#        test if int is even or odd
+#        if even, append to even.txt
+#        if odd, append to odd.txt
+#    close txt file
+#    if possible, make things fancy
+
