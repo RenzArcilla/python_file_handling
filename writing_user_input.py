7f87296f711c139f5ada1b5f10505f49882396b3
unknown <renzarcilla09@gmail.com>
2024-04-21 19:39:43 +0800

    major change in pseudocode, learned more about windows, planning to implement

--- writing_user_input.py
+++ writing_user_input.py
@@ -1,9 +1,8 @@
 # pseudocode:
-#     create while loop
-#         ask user whether or not to enter a statement
-#         if user answers yes:
-#              ask user to input a statement and store the statement to a variable
-#              open a file to write the statements into:
-#              append the value from the variable to the file
-#         if user answers yes:
-#              break while loop
+#     create a window
+#     display instructions as text on window
+#     create an entry widget in window for users to enter statements
+#     create button for user to submit their statement
+#     append the submitted statement on a txt file
+#     create an exit button that exits window and terminates the program
+#     make things fancy(resize, change bg and fg colors)
