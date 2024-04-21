748511ca4925a20ef7ae0bb6c43d2e72dd16dbe4
unknown <renzarcilla09@gmail.com>
2024-04-21 13:41:56 +0800

    altered pseudocode

--- read_and_write_even_or_odd.py
+++ read_and_write_even_or_odd.py
@@ -6,8 +6,11 @@
 #        strip "\n"
 #        convert str to int
 #        test if int is even or odd
-#        if even, append to even.txt
-#        if odd, append to odd.txt
+#        if even, append to string of even numbers
+#        if odd, append to string of odd numbers
+#    change color of both string
+#    append list of even numbers to even.txt
+#    append list of even numbers to even.txt
 #    close txt file
 #    if possible, make things fancy
 
@@ -16,10 +19,12 @@ with open("numbers.txt", "r") as numbers_file:
         line = line.rstrip("\n")
         line = int(line)
         if line % 2 == 0: #checks if int is even
-            with open("even.txt", "a") as even_numbers_file:
-                even_numbers_file.write(str(line))
-                even_numbers_file.write("\n")
+            pass
+#            with open("even.txt", "a") as even_numbers_file:
+#                even_numbers_file.write(str(line))
+#                even_numbers_file.write("\n")
         else: #checks if int is odd
-            with open("odd.txt", "a") as odd_numbers_file:
-                odd_numbers_file.write(str(line))
-                odd_numbers_file.write("\n")
+            pass
+#            with open("odd.txt", "a") as odd_numbers_file:
+#                odd_numbers_file.write(str(line))
+#                odd_numbers_file.write("\n")
