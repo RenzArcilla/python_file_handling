0ee0ec6949e15bc58022a3063883c79c9685456b
unknown <renzarcilla09@gmail.com>
2024-04-21 14:21:26 +0800

    unable to write str with color, appended odd and even numbers to their respective files

--- read_and_write_even_or_odd.py
+++ read_and_write_even_or_odd.py
@@ -8,9 +8,8 @@
 #        test if int is even or odd
 #        if even, append to string of even numbers
 #        if odd, append to string of odd numbers
-#    change color of both string
 #    append list of even numbers to even.txt
-#    append list of even numbers to even.txt
+#    append list of odd numbers to odd.txt
 #    close txt file
 #    if possible, make things fancy
 
@@ -24,5 +23,9 @@ with open("numbers.txt", "r") as numbers_file:
             even_numbers = even_numbers + ", " + str(line)
         else: #checks if int is odd
             odd_numbers = odd_numbers + ", " + str(line)
-even_numbers = "LIST OF EVEN NUMBERS: " + even_numbers.lstrip(", ")
-odd_numbers = "LIST OF ODD NUMBERS: " + odd_numbers.lstrip(", ")
+even_numbers = "LIST OF EVEN NUMBERS: " + even_numbers.lstrip(", ") + "\n"
+odd_numbers = "LIST OF ODD NUMBERS: " + odd_numbers.lstrip(", ") + "\n"
+with open("even.txt", "a") as even_numbers_file:
+    even_numbers_file.write(even_numbers)
+with open("odd.txt", "a") as odd_numbers_file:
+    odd_numbers_file.write(odd_numbers)
