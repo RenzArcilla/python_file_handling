b8572a740e24fa80f1662d4432affc7ab367d33b
unknown <renzarcilla09@gmail.com>
2024-04-21 16:59:14 +0800

    wrote pseudocode

---
+++ writing_user_input.py
@@ -0,0 +1,9 @@
+# pseudocode:
+#     create while loop
+#         ask user whether or not to enter a statement
+#         if user answers yes:
+#              ask user to input a statement and store the statement to a variable
+#              open a file to write the statements into:
+#              append the value from the variable to the file
+#         if user answers yes:
+#              break while loop
