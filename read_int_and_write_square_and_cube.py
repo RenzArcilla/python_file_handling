b354d76d3e07be88c49ece4b05758b040de65838
unknown <renzarcilla09@gmail.com>
2024-04-22 19:02:31 +0800

    wrote pseudocode

---
+++ read_int_and_write_square_and_cube.py
@@ -0,0 +1,15 @@
+# pseudocode:
+#     create input file, add 20 ints
+#     initialize 2 variables (for list of all squares and cubes) that will store a string
+#     open input file and read line by line
+#         store line as str in a variable
+#         remove unnecessary characters
+#         convert str to int
+#         compute for square and store in a variable
+#         compute for cube and store in a variable
+#         append square to the variable containing a string
+#         append cube to the other variable containing a string
+#     open txt output file for squared ints
+#         write the string containing all the squares
+#     open txt output file for squared ints
+#         write the string containing all the cubes
